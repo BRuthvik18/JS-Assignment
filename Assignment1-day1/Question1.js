8e9a7924128f4e0945f4634d364adadaea5d2c1f
BRuthvik18 <68223170+BRuthvik18@users.noreply.github.com>
2020-07-15 13:03:09 +0530

    Assignment 1
    question no 1

---
+++ Assignment1-day1/Question1.js
@@ -0,0 +1,10 @@
+/*EXPLORE AND EXPLAIN VARIOUS METHODS IN CONSOLE FUNCTION 
+-> CONSOLE.LOG(); It is a function in javascript which is used to print any kind of variables defined before in it or to just print any message that needs to be displayed
+EXAMPLES*/
+var a=2;
+console.log(a);
+var name='Ram';
+console.log(name);
+console.log('the value of a is', a);
+/* THE CONSOLE.WARN() METHOD IS USED TO WRITE A WARNING MESSAGE IN CONSOLE TO DISPLAY THE OUTPUT AS WARNING MESSAGE*/
+console.warn('this is a warning message');
