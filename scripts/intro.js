3ddc82b87d7c167e029fb4281a59ec6788e96c74
BRuthvik18 <68223170+BRuthvik18@users.noreply.github.com>
2020-07-17 13:44:26 +0530

    Day-3
    Question no 1

---
+++ scripts/intro.js
@@ -0,0 +1,13 @@
+console.log("hi");
+function evenodd()
+{
+let num=prompt("Enter a number");
+if (num%2==0) {
+    console.log(`It is even NUmber`);
+    
+} else {
+    console.log(`The number entered is ${num} and it is Odd` );
+    
+}    
+}
+evenodd();
