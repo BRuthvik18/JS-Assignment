3b81975c42d70dc02dd5f402bd3ede1e9016bfda
BRuthvik18 <68223170+BRuthvik18@users.noreply.github.com>
2020-07-20 16:27:21 +0530

    Day 4 Assignments
    Question 5

--- scripts/Calculator.js
+++ scripts/Calculator.js
@@ -1,61 +1,33 @@
-let i=+prompt(`Press 1 for Addition,press 2 for subtraction,press 3 for multiplication,press 4 Division,press 5 for Square root,press 6 for Percentage`);
-let a,b;
-switch (true) {
-    case i==1:
+let i =+prompt("Enter the sales value to get the commission");
+switch (true)
+ {
+    case ( i>0  && i<=5000):
     {
-        a=+prompt(`enter the number1 `);
-        b=+prompt(`enter the number2`);
-    sum=a+b;
-     console.log(sum);
+          let sum=(i*2)/100;
+          console.log(`=>${sum} is your commission`);
     }
         break;
-    case i==2:
+    case (i>5001 && i<=10000):
         {
-        a=+prompt(`enter the number1 `);
-        b=+prompt(`enter the number2`);
-        if(a>b)
-        {
-            sub=a-b;
-            console.log(sub);
+            let sum1=(i*5)/100;
+          console.log(`${sum1} is your commission`);
         }
-        else
-        {
-           sub=b-a;
-           console.log(sub);
-        }
-        }
-          break;
-        case i==3:
-            {
-                a=+prompt(`enter the number1 `);
-                b=+prompt(`enter the number2`);
-                mul=a*b;
-                console.log(mul);
-            }
-            break;
-        case i==4:
-            {
-                a=+prompt(`enter the number1 `);
-                b=+prompt(`enter the number2`);
-                div=a/b;
-                console.log(div);
-            }
-            break;
-        case i==5:
-            {
-                a=+prompt(`enter the number1 `);
-                console.log(Math.sqrt(a));
-            }
-            break;
-        case i==6:
-            {
-            a=+prompt(`The number you have to calculate from the total to get percentage`);
-            b=+prompt(`the total number from which it should becalculated`);
-            per=(a/b)*100;
-            console.log(`${per}%`);
-            }
-            break;
-    default:
-        console.log(" Be Happy");
         break;
+    case (i>10000  && i<=20000):
+        {
+            let sum2=(i*7)/100;
+          console.log(`${sum2} is your commission`);
+        }
+        break;
+    case (i>=20001):
+        {
+            let sum3=(i*10)/100;
+          console.log(`${sum3} is your commission`);
+        }
+        break;
+
+    default:
+        {
+            console.log("BE HAPPY");
+        }
 }
