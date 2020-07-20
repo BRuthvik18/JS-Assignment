7efd321f00aeb5c529449ca8c02b3545767ece78
BRuthvik18 <68223170+BRuthvik18@users.noreply.github.com>
2020-07-20 12:21:38 +0530

    Day -4 Assignment
    Question 1

---
+++ scripts/loops.js
@@ -0,0 +1,17 @@
+for(let i=0;i<=100;i++)
+{
+    if (i%3==0) 
+    {
+     console.log(`fizz =${i}`);   
+    }
+     if(i%5==0)
+    {
+        console.log(`Buzz =${i}`);
+    }
+     if(i%3==0 && i%5==0)
+    {
+       console.log(`FizzBuzz = ${i}`);
+    }
+        
+    
+}
